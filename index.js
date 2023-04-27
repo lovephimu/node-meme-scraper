610cdc991f37b665cc4a96deeaf3ef6c866e9839
lovephimu <soe.nlb@gmail.com>
2023-04-27 15:38:31 +0200

    add download function

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
+import fs from 'node:fs'; // import fs module to read or write files
+import client from 'node:https';
+
 const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/'; // the memepage Url
+const folderPath = './memes';
 
 // fetch function for HTML & possibly more (the images)
-// send request to parameter (a url)
-// check if response is okay and not null return HTML in string format
+// translating response into text and saving it in responseText
 
-async function getStuff(url) {
-  await fetch(url)
-    .then(
-      // insert an if statement here to separate between website url and img url
-      (response) => {
-        if (response.ok) {
-          return response.text();
-        }
-        throw new Error('Boom! Response not okay!'); // just in case the response is not okay
-      }, // ,
-      // (networkError) => {
-      //  console.log(networkError.message);
-      // },
-    )
-    .then((data) => {
-      const regEx = /https:\/\/.*?\.jpg/g; // regular expression to filter all adresses starting with http and ending with .jpg
+const response = await fetch(memeUrl);
+const responseText = await response.text();
 
-      const matches = data.match(regEx).slice(0, 10); // match for strings according to regex pattern but only the first 10
-      console.log(matches);
-    })
-    .catch((error) => console.log(error.message));
+// defining function declaration for filtering the responseText for image urls by using regex
+
+function filterImageUrls(data) {
+  const regEx = /https:\/\/.*?\.jpg/g; // regular expression for images urls
+
+  const matches = data.match(regEx).slice(0, 10); // match according to regEx pattern
+  return matches;
 }
 
-await getStuff(memeUrl); // this await should not work but lint forces me to? :D
+// saving filtered image urls to an array
+
+const filteredUrls = filterImageUrls(responseText);
+console.log(filteredUrls);
+
+// defining function declaration for downloading images
+
+function downloadImage(urls, filepath) {
+  client.get(urls, (res) => {
+    res.pipe(fs.createWriteStream(filepath));
+  });
+}
+
+// async function getStuff(url) {
+//   await fetch(url)
+//     .then(
+//       // insert an if statement here to separate between website url and img url
+//       (response) => {
+//         if (response.ok) {
+//           return response.text();
+//         }
+//         throw new Error('Boom! Response not okay!'); // just in case the response is not okay
+//       },
+//     )
+//     .then((data) => {
+//       const regEx = /https:\/\/.*?\.jpg/g; // regular expression for images urls
+
+//       const matches = data.match(regEx).slice(0, 10); // match according to regEx pattern
+//       console.log(matches);
+//       return matches;
+//     })
+//     .catch((error) => console.log(error.message));
+// }
+
+// console.log(await getStuff(memeUrl));
 
 // fetch('https://memegen-link-examples-upleveled.netlify.app/');
+
+// await fetch('https://api.memegen.link/images/keanu.jpg').then((result) =>
+//   console.log(result),
+// );
