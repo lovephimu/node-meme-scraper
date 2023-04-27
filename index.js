1106e2096e27da5cb96b95761947c49b8f68b193
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-04-27 14:21:02 +0200

    add comments

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/'; // the memepage Url
 
-async function getStuff(url) {
-  // fetch function for HTML & possibly more (the images)
+// fetch function for HTML & possibly more (the images)
+// send request to parameter (a url)
+// check if response is okay and not null return HTML in string format
 
-  await fetch(url) // send request to parameter (a url)
+async function getStuff(url) {
+  await fetch(url)
     .then(
+      // insert an if statement here to separate between website url and img url
       (response) => {
         if (response.ok) {
           return response.text();
         }
-        throw new Error('Boom! Response not okay!');
-      },
-      (networkError) => {
-        console.log(networkError.message);
-      },
+        throw new Error('Boom! Response not okay!'); // just in case the response is not okay
+      }, // ,
+      // (networkError) => {
+      //  console.log(networkError.message);
+      // },
     )
     .then((data) => {
       const regEx = /https:\/\/.*?\.jpg/g; // regular expression to filter all adresses starting with http and ending with .jpg
 
-      const matches = data.match(regEx);
-      console.log(matches.slice(0, 10));
+      const matches = data.match(regEx).slice(0, 10); // match for strings according to regex pattern but only the first 10
+      console.log(matches);
     })
     .catch((error) => console.log(error.message));
 }
